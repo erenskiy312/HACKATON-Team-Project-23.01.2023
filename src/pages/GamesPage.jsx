a5260eda78e0107d4c58af6620eb2701ca090f72
aidar <zefircatitypoigogo@gmail.com>
2023-01-30 16:34:59 +0600

    add pagination

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,6 +1,7 @@
 import { Box, Grid } from "@mui/material";
 import React from "react";
 import GameCard from "../components/Navbar/game/GameCard";
+import GameList from "../components/Navbar/game/GameList";
 import SideBar from "../components/Navbar/game/SideBar";
 
 const GamesPage = () => {
@@ -9,6 +10,7 @@ const GamesPage = () => {
       <Box sx={{ marginTop: "5rem" }}>
         <Grid container spacing={3}>
           {/* <GameCard /> */}
+          <GameList />
           <SideBar />
         </Grid>
       </Box>
